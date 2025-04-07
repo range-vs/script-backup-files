276cf89c442b0540e4b7f95daab3680742caba7f
range_vs <lagosmod@gmail.com>
2025-04-07 22:37:33 +0300

    add test smb & key bac service

--- bac.sh
+++ bac.sh
@@ -5,15 +5,28 @@ DEST_DIR="YOUR_DEST_DIR"
 DEST_SMB_DIR="YOUR_DEST_SAMBA_DIR"
 TOKEN="YOUR_TG_BOT_TOKEN"
 CHAT_ID="YOUR_TG_CHAT_ID"
+KEY="[BACKUP_SERVICE]"
+
+# test connect to smb
+SMB_MOUNTS=$(mount | grep smb)
+
+if [ -z "$SMB_MOUNTS" ]; then
+    CURL_OUTPUT_SMB=`curl -s -X POST "https://api.telegram.org/bot${TOKEN}/sendMessage" \
+        -d chat_id="${CHAT_ID}" \
+        -d text="${KEY} Error test connect to smb. Break"`
+    echo "$CURL_OUTPUT_SMB"
+    echo "${KEY}Error test connect to smb. Break"
+    exit 1
+fi
 
 # create arch music
 if [ ! -d "$DEST_DIR" ]; then
-  echo "Error: folder $DEST_DIR is not exist!"
+  echo "${KEY} Error: folder $DEST_DIR is not exist!"
   exit 1
 fi
 
 if [ ! -d "$SOURCE_DIR" ]; then
-  echo "Error: folder $SOURCE_DIR is not exist!"
+  echo "${KEY} Error: folder $SOURCE_DIR is not exist!"
   exit 1
 fi
 
@@ -25,12 +38,12 @@ ls -t | grep -E 'music_bec_.*\.zip' | sed -n '4,$p' | xargs -d '\n' rm -f --
 
 # copy to smb
 if [ ! -f "$DEST_DIR/$ZIP_NAME" ]; then
-  echo "Error: file $DEST_DIR/$ZIP_NAME is not exist!"
+  echo "${KEY} Error: file $DEST_DIR/$ZIP_NAME is not exist!"
   exit 1
 fi
 
 if [ ! -d "$DEST_SMB_DIR" ]; then
-  echo "Error: folder $DEST_SMB_DIR is not exist!"
+  echo "${KEY} Error: folder $DEST_SMB_DIR is not exist!"
   exit 1
 fi
 
@@ -48,14 +61,14 @@ BACKUPS_SMB_LIST=$(ls -lh --time-style=long-iso "$DEST_SMB_DIR"/music_bec_*.zip 
 
 MESSAGE="range music backup ok, date ${TIMESTAMP}, size file ${FILE_SIZE}%0Alist backups:%0A${BACKUPS_LIST}%0Alist backups:%0A${BACKUPS_SMB_LIST}"
 
-echo "Final msg copy output"
-echo "${MESSAGE}"
+echo "${KEY} Final msg copy output"
+echo "${KEY} ${MESSAGE}"
 
 CURL_OUTPUT=`curl -s -X POST "https://api.telegram.org/bot${TOKEN}/sendMessage" \
     -d chat_id="${CHAT_ID}" \
     -d text="${MESSAGE}"`
 
-echo "Curl output"
-echo "$CURL_OUTPUT"
+echo "${KEY} Curl output"
+echo "${KEY} $CURL_OUTPUT"
 
 # TODO: send error to tg
